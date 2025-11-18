a4ce0396a3be1b5af274fc61c21975551ab1f03d
RahmetAhmed <159233912+RahmetAhmed@users.noreply.github.com>
2025-11-18 16:12:09 +0300

    add rahmet

--- source/js/_functions.js
+++ source/js/_functions.js
@@ -7,7 +7,7 @@ import { mobileCheck } from "./functions/mobile-check";
 // if (isDesktop()) {
 //   console.log('...')
 // }
-
+//hi rahmet
 // Троттлинг функции (для ресайза, ввода в инпут, скролла и т.д.)
 // import { throttle } from './functions/throttle';
 // let yourFunc = () => { console.log('throttle') };
